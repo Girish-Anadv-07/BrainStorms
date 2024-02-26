0cc54d985b6d9c48a702068f4d2915ad63db5622
Girish <79399756+Girish-Anadv-07@users.noreply.github.com>
2024-02-26 03:58:22 -0500

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -1,11 +1,11 @@
 import os
-rootdir = "../wikifiles"
+rootdir = "./"
 with open("_Sidebar.md", "w") as file:
     file.write("# **Projects**"+"\n\n")
     for subdir, dirs, files in os.walk(rootdir):    
         print(subdir)
-        if subdir[13:]!='':
-            file.write("* ## **"+ subdir[13:] +"**" + "\n")
+        if subdir[2:]!='':
+            file.write("* ## **"+ subdir[2:] +"**" + "\n")
             print(subdir)
         for file_name in files:
             file.write("\t"+"* ["+ file_name[:-3] + "](" + file_name[:-3].replace(" ", "%20") + ")" + "\n")
