1755f8786cccea0cfd6c12cbd29ac655a7ba9080
Girish Mittapalle <gm@Girishs-MacBook-Air.local>
2024-02-26 04:06:07 -0500

    -

--- sidebar.py
+++ sidebar.py
@@ -3,10 +3,8 @@ rootdir = "../wikifiles"
 with open("_Sidebar.md", "w") as file:
     file.write("# **Projects**"+"\n\n")
     for subdir, dirs, files in os.walk(rootdir):    
-        print(subdir)
         if subdir[13:]!='':
             file.write("* ## **"+ subdir[13:] +"**" + "\n")
-            print(subdir)
         for file_name in files:
             file.write("\t"+"* ["+ file_name[:-3] + "](" + file_name[:-3].replace(" ", "%20") + ")" + "\n")
     file.close()
