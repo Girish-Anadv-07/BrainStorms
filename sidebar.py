8dbd2007d667b655d2ad02779b0a1b53bbf273ee
Girish Mittapalle <gm@Girishs-MacBook-Air.local>
2024-02-26 03:21:36 -0500

    bug fix

--- sidebar.py
+++ sidebar.py
@@ -1,11 +1,11 @@
 import os
-rootdir = "./"
+rootdir = "../wikifiles"
 with open("_Sidebar.md", "w") as file:
     file.write("# **Projects**"+"\n\n")
     file.write("***"+"\n\n")
     for subdir, dirs, files in os.walk(rootdir):    
-        if subdir[12:]!='':
-            file.write("* ## **"+ subdir[12:] +"**" + "\n")
+        if subdir[13:]!='':
+            file.write("* ## **"+ subdir[13:] +"**" + "\n")
             print(subdir)
         for file_name in files:
             file.write("\t"+"* ### ["+ file_name[:-3] + "](" + file_name[:-3].replace(" ", "%20") + ")" + "\n")
